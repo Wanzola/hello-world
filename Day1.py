8b4e649330a5982737f68330ab59872e0696dcaf
Wanzola <56655187+Wanzola@users.noreply.github.com>
2019-10-20 20:09:01 -0500

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,7 @@
+print("Python is Fun")
+print("This is an example to display display backslash character \ ")
+print("Hello\nWorld")
+val1 = 5
+val2 = 8
+sum = float(val1) + float(val2)
+print("The sum of given numbers is: ", sum)
